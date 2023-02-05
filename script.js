78d40857d27adf33a28bd399fb1b5610a9fe4fcd
Aghanim27 <khotheagh27798@gmail.com>
2023-02-06 00:16:50 +0800

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (let i= 0; i < names.length; i++) {
-    let firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === "j") {
-        console.log("Hello " + names[i]);
-    }
-    else {
-        console.log("Goodbye " + names[i]);
-    }
+let container = document.getElementsByClassName("container")[0];
+let myButton = document.getElementsByClassName("myButton")[0];
+
+let greeting = () => {
+    let person = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    let nameDiv = document.createElement("div");
+
+    for (let i = 0; i < person.length; i++) {
+        let firstLetter = person[i].charAt(0);
+        if (firstLetter === "J") {
+            let startJ = "Good Bye "+ person[i]+ " / ";
+            nameDiv.append(startJ)
+        } else {
+            let startNoJ = "Hello "+ person[i]+ " / ";
+            nameDiv.append(startNoJ);
+        }
+    };
+    container.append(nameDiv);
 }
+
+myButton.addEventListener("click",greeting)
